25a0f683d3f298a18ffff0a1a682df9da259dd1c
aizem-sama <71040136+israel2705@users.noreply.github.com>
2025-03-29 20:02:14 -0300

    israel java 29/03 domingo

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-//aqui vamos colocar de inicio apenas funcoes que verifiquem se os campos foram preenchidos corretamente 
-//comecaremos criando as variaveis constantes ----- EX: const verificar e utilizando get.Element e outros para criar nosso codigo
+
+const form =document.getElementById('form');
+const campos=document.querySelectorAll('.informacoes-participante-pessoais');
+const spans=document.querySelectorAll('.error');
+const emailRegex= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+//colocar uma função para cada span, aqui a função terá o mesmo código javascrip para todos pois os spans ja existem no index.html so que deixei eles ocultos
+// precisamos deixar o formulário responsivo colocando a responsividade no css ou utilizando o booststrap
+
+
